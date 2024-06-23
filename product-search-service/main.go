4a2c9a774d241a6962ab9efb5e8564da508db55f
zem-1 <diogozem21@gmail.com>
2024-06-23 04:21:37 -0300

    Front, API
    Pagina de front com animação de background, não deu para colocar foto nos produtos. Mudança na API, inseri o CORS e agora a busca está funcionando com palavra/termo chave

--- product-search-service/main.go
+++ product-search-service/main.go
@@ -7,6 +7,7 @@ import (
 	"product-search-service/handlers"
 
 	"github.com/gorilla/mux"
+	"github.com/rs/cors"
 )
 
 func main() {
@@ -14,6 +15,16 @@ func main() {
 	r.HandleFunc("/search", handlers.SearchProducts).Methods("GET")
 	r.HandleFunc("/suggestions", handlers.SuggestProducts).Methods("GET")
 
+	//Configurar o middleware CORS
+	c := cors.New(cors.Options{
+		AllowedOrigins:   []string{"*"}, // Permitir todas as origens, você pode restringir isso conforme necessário
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE"},
+		AllowedHeaders:   []string{"Content-Type", "Authorization"},
+		AllowCredentials: true,
+	})
+
+	handler := c.Handler(r)
+
 	log.Println("Starting server on :8080")
-	log.Fatal(http.ListenAndServe(":8080", r))
+	log.Fatal(http.ListenAndServe(":8080", handler))
 }
